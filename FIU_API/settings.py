03b2ba7d81d0d3a60c3bac894b206cd7bb7fe4c3
Akash Joshi <akashrockzz411@gmail.com>
2021-10-07 14:15:30 +0530

    Herku deploy

--- FIU_API/settings.py
+++ FIU_API/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b=^xwfw&y63c(g3j#l5-tno2z0&il*12&qq2pga!2&q*m0m0%d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 STATIC_ROOT = BASE_DIR / 'static'
 
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
